e91243fadcba75c43afcace084d2aa355567bec3
Kai <kimobrie@ucsc.edu>
2022-02-28 18:15:01 -0800

    defense works on baseline, is not great though and will improve for competition

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -61,13 +61,14 @@ class Defense(CaptureAgent):
     You should look at `pacai.core.baselineTeam` for more details about how to create an agent.
     """
 
-    def __init__(self, foodDist, attackDist, gameState, numCapsules, index, **kwargs):
+    def __init__(self, red, blue, food, index, **kwargs):
         super().__init__(index, **kwargs)
-
-        self.foodDist = -float("inf")
-        self.attackDist = -float("inf")
-        self.gameState = gameState
-        self.numCapsules = numCapsules
+        # checks which team self is on
+        if self.isOnRedTeam(index):
+            self.red = true
+        if self.isOnBlueTeam(index):
+            self.blue = true
+        self.food = getFoodYouAreDefending(self).asList()
 
     def registerInitialState(self, gameState):
         """
@@ -76,16 +77,39 @@ class Defense(CaptureAgent):
 
         IMPORTANT: If this method runs for more than 15 seconds, your agent will time out.
         """
-
         super().registerInitialState(gameState)
 
         # Your initialization code goes here, if you need any.
-        
-
+        # initalizes a list of opponents on our side (aka dangerous opponents)
+        dangerousOpponents = ()
+        for op in getOpponents(gameState):
+            if self.red and isOnRedSide(op):
+                dangerousOpponents.append(op)
+            elif self.blue and isOnBlueSide(op):
+                dangerousOpponents.append(op)
+        # the most dangerous opponent is on our side and closest to our food
+        # the most in danger capsule is the capsule closest to the opponent
+        closestDist = -float("inf")
+        mostDangerousOp = None
+        for op in dangerousOpponents:
+            for foo in self.food:
+                if closestDist > getDistance(op, foo):
+                    closestDist = getDistance(op, foo)
+                    mostDangerous = op  # the most dangerous opponent
     def chooseAction(self, gameState):
         """
         Randomly pick an action.
         """
-
         actions = gameState.getLegalActions(self.index)
+        # if none on our side, move toward food near middle or patrol around
+        if len(dangerousOpponents) > 0:
+            d = float("inf")
+            bestAction = None
+            for action in actions:
+                suc = self.getSuccessor(gameState, action)
+                if getDistance(suc, mostDangerous) < d
+                    d = getDistance(suc, mostDangerous)
+                    bestAction = action
+            if bestAction not None:
+                return bestAction
         return random.choice(actions)
