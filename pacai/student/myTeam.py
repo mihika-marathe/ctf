29ecaf6f98419c92516c687bf6b5faa60f583fe8
rlchoy <rlchoy@ucsc.edu>
2022-03-05 21:11:29 -0800

    edited offense agent and defense agent (a little)

--- pacai/student/myTeam.py
+++ pacai/student/myTeam.py
@@ -38,6 +38,10 @@ class Offense(ReflexCaptureAgent):
     def __init__(self, index, **kwargs):
         super().__init__(index)
 
+        self.prev = [] # keeps track of previous moves to see if we're stuck somewhere?
+        self.capsules = []
+        self.food = []
+
     def getFeatures(self, gameState, action):
         features = {}
         successor = self.getSuccessor(gameState, action)
@@ -45,7 +49,9 @@ class Offense(ReflexCaptureAgent):
 
         # Compute distance to the nearest food.
         foodList = self.getFood(successor).asList()
+        self.food = foodList
         capsuleList = self.getCapsules(gameState)
+        self.capsules = capsuleList
         enemies = [successor.getAgentState(i) for i in self.getOpponents(successor)]
         invaders = [a.getPosition() for a in enemies]
 
@@ -75,6 +81,7 @@ class Offense(ReflexCaptureAgent):
         Picks among the actions with the highest return from `ReflexCaptureAgent.evaluate`.
         """
 
+        # legal actions of our offense agent
         actions = gameState.getLegalActions(self.index)
 
         start = time.time()
@@ -82,8 +89,10 @@ class Offense(ReflexCaptureAgent):
         # determines if ghost is next to them when an action is taken
         values = []
         op = self.getOpponents(gameState)
+        eat_food = False
+        being_followed = False
         for a in actions:
-            # slug trap win condition = no stop?
+            # slug trap win condition = no stop
             if (a != Directions.STOP):
                 suc = self.getSuccessor(gameState, a)
                 bad_pos = False
@@ -92,27 +101,63 @@ class Offense(ReflexCaptureAgent):
                     # small incentive to get rid of ghosts on our side? or just completely ignore them
                     op_state = suc.getAgentState(op[i])
                     self_state = suc.getAgentState(self.index)
-                    if (op_state.isScaredGhost() is False or (op_state.isPacman() is True and self_state.isScaredGhost is False)):
-                        next_pos = suc.getAgentState(self.index).getPosition()
+                    # don't avoid ghosts if the enemy ghosts are scared or
+                    # the enemy is pacman on our side of the board and we are not scared ghosts
+                    if (op_state.isScaredGhost() is True or (op_state.isPacman() is False and self_state.isScaredGhost() is True)):
+                        next_pos = self_state.getPosition()
                         op_pos = gameState.getAgentState(op[i]).getPosition()
                         # difference between pos if action is taken and current pos
                         dist = self.getMazeDistance(next_pos, op_pos)
                         if (dist <= 1):
                             bad_pos = True
+                            being_followed = True
+                        '''
+                        # if op ghost is not in the way of the food
+                        # determines if next pos will lead to food being eaten
+                        else:
+                            for i in self.food:
+                                if i == next_pos:
+                                    eat_food = True
+                        '''
+
                 # if action taken doesn't collide with a ghost
                 if (bad_pos is False):
+                    # if the successor state doesn't lead into a dead end 
+                    # if (self_state.getLegalActions(self.index) > 2):
                     values.append((a, self.evaluate(gameState, a)))
 
         bestVal = -999999
         bestAction = None
+
+        '''
+        # randomization only if there aren't any moves that will lead to food being eaten
+        if (gameState.getAgentState(self.index).isPacman() and eat_food is False):
+            if (flipCoin(0.1) and len(values) != 0):
+                x = random.choice(values)
+                return x[0]
+        '''
+
+        # determines if we're stuck in a tunnel and should die to save time
+        # only one action besides stop
+        if (len(values) == 1 and being_followed is True and gameState.getAgentState(self.index).isPacman()):
+            self.stuck += 1
+        else:
+            self.stuck = 0
+        if (self.stuck > 5):
+            return random.choice(actions)
+
+        # determines best action otherwise
         for i in range(len(values)):
             if (values[i][1] > bestVal):
                 bestVal = values[i][1]
                 bestAction = values[i][0]
         if bestAction != None:
+            self.prev.append(bestAction)
             return bestAction
         else:
-            return random.choice(actions)
+            x = random.choice(actions)
+            self.prev.append(x)
+            return x
 
 class Defense(ReflexCaptureAgent):
     """
@@ -166,6 +211,16 @@ class Defense(ReflexCaptureAgent):
         """
 
         actions = gameState.getLegalActions(self.index)
+        op = self.getOpponents(gameState)
+        
+        for i in op:
+            op_actions = gameState.getLegalActions(i)
+            if gameState.getAgentState(i).isPacman() is True and gameState.getAgentState(self.index).isScaredGhost() is False and len(op_actions) <= 2:
+                return Directions.STOP
+            op_actions.clear()
+
+        # trap the enemy pacman if you're not a scared ghost
+        # is trapped if enemy pacman has only 2 legal moves: into our defense agent or stop
 
         start = time.time()
         values = [self.evaluate(gameState, a) for a in actions]
